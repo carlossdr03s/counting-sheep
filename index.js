796f7ca6ca99f14785710b0b0438b034ae2946c3
carlossdr03s <carlossodreibm@gmail.com>
2023-09-11 10:12:33 -0300

    feat: working code

---
+++ index.js
@@ -0,0 +1,20 @@
+function countSheeps(sheep) {
+    let count = 0;
+    for (let sheepIndex = 0; sheepIndex < sheep.length; sheepIndex++) {
+        const isSheepPresent = sheep[sheepIndex];
+        if (isSheepPresent) {
+            count++;
+        }
+    } 
+    return count
+  }
+
+
+console.log(countSheeps([
+    true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true
+]));
